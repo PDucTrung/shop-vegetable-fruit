0c7352c40c5370321f573da58f73ec92c770f5bc
trung <trungezeal115@gmail.com>
2022-10-04 14:39:03 +0700

    dd

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import $ from "jquery";
+import "hover.css/css/hover.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "animate.css/animate.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick.min.js";
+import "swiper/swiper.min.css";
+import "swiper/swiper-bundle.min.js";
+import "../css/global.css";
+import "../css/index.css";
+
+$("#app").html("<h1>Hello world</h1>");
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "./src",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+        about: resolve(__dirname, "src/about.html"),
+        product: resolve(__dirname, "src/product.html"),
+      },
+    },
+  },
+});
