7834e4dcd82bdb37cc44a4c45e09694cd09ac7be
trung <trungezeal115@gmail.com>
2022-10-07 01:32:39 +0700

    done shop

--- src/js/index.js
+++ src/js/index.js
@@ -405,3 +405,54 @@ $(".slider-partners").slick({
     },
   ],
 });
+
+// sldier list product /////////////////////////////////////////////////////////////////////////////////////////////////
+// slider about ////////////////////////////////////////////////////////
+$(".slider-fruit").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 1000,
+  dots: true,
+  fade: true,
+  cssEase: "linear",
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-arrow-right' aria-hidden='true'></i></button>",
+});
+
+// filter range ////////////////////////////////////////////////////////////////////////////////////////////////////
+var lowerSlider = document.querySelector("#lower");
+var upperSlider = document.querySelector("#upper");
+
+document.querySelector("#two").value = upperSlider.value;
+document.querySelector("#one").value = lowerSlider.value;
+
+var lowerVal = parseInt(lowerSlider.value);
+var upperVal = parseInt(upperSlider.value);
+
+upperSlider.oninput = function () {
+  lowerVal = parseInt(lowerSlider.value);
+  upperVal = parseInt(upperSlider.value);
+
+  if (upperVal < lowerVal + 4) {
+    lowerSlider.value = upperVal - 4;
+    if (lowerVal == lowerSlider.min) {
+      upperSlider.value = 4;
+    }
+  }
+  document.querySelector("#two").value = this.value;
+};
+
+lowerSlider.oninput = function () {
+  lowerVal = parseInt(lowerSlider.value);
+  upperVal = parseInt(upperSlider.value);
+  if (lowerVal > upperVal - 4) {
+    upperSlider.value = lowerVal + 4;
+    if (upperVal == upperSlider.max) {
+      lowerSlider.value = parseInt(upperSlider.max) - 4;
+    }
+  }
+  document.querySelector("#one").value = this.value;
+};
