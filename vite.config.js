8a92a6c966299c02ab0e9f790766015d9de532a6
trung <trungezeal115@gmail.com>
2022-10-07 19:12:56 +0700

    dasd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "./src",
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
         index: resolve(__dirname, "src/index.html"),
