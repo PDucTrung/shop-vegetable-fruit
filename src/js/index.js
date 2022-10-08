766a5789d5cb6560a5a0be03ee66d48799c5b744
trung <trungezeal115@gmail.com>
2022-10-08 18:06:16 +0700

    asa

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,7 @@ import "../css/index.css";
 import "../css/global.css";
 import "../css/product.css";
 import "../css/cart.css";
+import "../css/checkout.css";
 
 // Page loader //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 $(window).on("load", function () {
