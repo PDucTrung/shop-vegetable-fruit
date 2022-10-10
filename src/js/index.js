5ab501eb07d9879789f94a1195fc37ed19828944
trung <trungezeal115@gmail.com>
2022-10-11 01:22:38 +0700

    dfd

--- src/js/index.js
+++ src/js/index.js
@@ -663,6 +663,39 @@ $(".slider-list-blog").slick({
   dots: true,
 });
 
+// Toast function
+
+const add = document.getElementById("add");
+let toast = document.querySelector(".toast");
+let closeIcon = document.querySelector(".close-add");
+let progress = document.querySelector(".progress");
+
+let timer1, timer2;
+
+add.addEventListener("click", () => {
+  toast.classList.add("active");
+  progress.classList.add("active");
+
+  timer1 = setTimeout(() => {
+    toast.classList.remove("active");
+  }, 5000); //1s = 1000 milliseconds
+
+  timer2 = setTimeout(() => {
+    progress.classList.remove("active");
+  }, 5300);
+});
+
+closeIcon.addEventListener("click", () => {
+  toast.classList.remove("active");
+
+  setTimeout(() => {
+    progress.classList.remove("active");
+  }, 300);
+
+  clearTimeout(timer1);
+  clearTimeout(timer2);
+});
+
 // call api
 
 // const axios = require("axios");
