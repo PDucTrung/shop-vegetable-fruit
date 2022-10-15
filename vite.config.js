b608ddabe9d68c64d813da0e0e3a21f2dbbf7788
trung <trungezeal115@gmail.com>
2022-10-16 00:01:39 +0700

    dsad

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./src",
-  publicDir: "assets",
+  publicDir: "img",
   build: {
     outDir: "dist",
     rollupOptions: {
