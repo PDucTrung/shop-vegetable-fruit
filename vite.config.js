803a8ba4929589f8e618faae2da01ee100d8042d
trung <trungezeal115@gmail.com>
2022-10-16 00:05:09 +0700

    dasd

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./src",
-  publicDir: "img",
+  publicDir: "assets",
   build: {
     outDir: "dist",
     rollupOptions: {
